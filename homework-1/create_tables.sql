68156fbfdd4b5c0053e62ad1831df8e9ab5c0899
avdeevaa <tess-avdeeva@seznam.cz>
2023-10-19 15:54:52 +0200

    First commit + push/HW results: code for three tables in create_tables.sql, code in main.py, it works in pgAdmin4

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,33 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id char(5),
+	company_name varchar(50),
+	contact_name varchar(50)
+);
+
+SELECT * FROM customers;
+
+CREATE TABLE employees
+(
+	employee_id int,
+	first_name varchar(30),
+	last_name varchar(30),
+	title varchar(30),
+	birth_date date,
+	notes text
+
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE orders
+(
+	order_id int,
+	customer_id char(5),
+	employee_id int,
+	order_date date,
+	ship_city text
+)
+
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+import csv
+
+connect = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="1234"
+)
+cursor = connect.cursor()
+
+#без этого не получается получить файлы в папке north data
+customers = r"C:\Users\avdeevaa\PycharmProjects\postgres-homeworks\homework-1\north_data\customers_data.csv"
+employees = r"C:\Users\avdeevaa\PycharmProjects\postgres-homeworks\homework-1\north_data\employees_data.csv"
+orders = r"C:\Users\avdeevaa\PycharmProjects\postgres-homeworks\homework-1\north_data\orders_data.csv"
+
+with open(customers) as file: #первая таблица
+    cust = csv.reader(file)
+    next(cust) # without this I have such error psycopg2.errors.StringDataRightTruncation: value too long for type character(7)
+    for x in cust:
+        cursor.execute('INSERT INTO customers VALUES (%s, %s, %s)', (x[0], x[1], x[2]))
+
+connect.commit() #sending it to pgAdmin4
+
+with open(employees) as file: #вторая таблица
+    empl = csv.reader(file)
+    next(empl)
+    for x in empl:
+        cursor.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (x[0], x[1], x[2], x[3], x[4], x[5]))
+
+connect.commit() #sending second to pgAdmin4
+
+with open(orders) as file: #третья таблица
+    ord = csv.reader(file)
+    next(ord)
+    for x in ord:
+        cursor.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (x[0], x[1], x[2], x[3], x[4]))
+
+connect.commit() #sending third to pgAdmin4
+
